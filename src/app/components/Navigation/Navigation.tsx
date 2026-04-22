158a96d87771b45d2c94daa954a2d5a51720944e
CelesteEvertsen <evertsen.celeste@gmail.com>
2026-04-22 20:03:01 +0200

    La til logo, fornøyd med plassering for mobil og desc

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,9 +1,10 @@
 import style from "./Navigation.module.css";
+import Image from "next/image";
 
 export default function Navigation() {
   return (
     <nav className={style.navContainer}>   
-        <h1>Logo</h1>
+        <Image src='/CE-logo-x1.png' alt="Celeste Evertsen" width={400} height={100} className={style.logo}/>
         <ul className={style.navList}>
             <li>Home</li>
             <li>About</li>
